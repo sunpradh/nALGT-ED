67af2e00c1c6296b10b59c1c691bbdd9b38ba059
Sunny Pradhan <sun.pradh@gmail.com>
2022-10-07 17:31:10 +0200

    added class Basis for the physical Hilbert space, it can list all the gauge invariant states

--- basis/basis.py
+++ basis/basis.py
@@ -62,3 +62,45 @@ def physical_basis(
         if len(inv_spaces) == len(vertices):
             basis.update({conf: inv_spaces})
     return basis
+
+
+class Basis:
+    def __init__(
+            self,
+            group: Group,
+            irreps: Irreps,
+            vertices: list[VertexLinks],
+            nlinks: int
+        ):
+        self.group = group
+        self.irreps = irreps
+        self.vertices = vertices
+        self.nlinks = nlinks
+        self._basis = self.physical_basis()
+        self.states = self._expand_basis()
+
+    def _expand_basis(self):
+        states_list = []
+        for irrep_conf in self._basis.keys():
+            inv_space_sizes = [len(v) for v in self._basis[irrep_conf]]
+            iterators = [range(size) for size in inv_space_sizes]
+            states_list += [(irrep_conf, A) for A in product(*iterators)]
+        return states_list
+
+    def physical_basis(self) -> dict[IrrepConf, list[InvariantSpace]]:
+        """
+        Compute the physical Hilbert space, given `group` and `irreps`, the links
+        of each vertex (`vertices`) and the number of links
+        """
+        possible_irrep_confs = product(range(len(self.irreps)), repeat=self.nlinks)
+        vbasis = vertex_basis(self.group, self.irreps)
+        basis = dict()
+        for conf in possible_irrep_confs:
+            inv_spaces = [
+                vbasis[vertex_conf(conf, vertex)]
+                    for vertex in self.vertices
+                    if vertex_conf(conf, vertex) in vbasis
+            ]
+            if len(inv_spaces) == len(self.vertices):
+                basis.update({conf: inv_spaces})
+        return basis
