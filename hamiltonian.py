8c36c539493950e2e7145b456b7c0a5a6b852668
Sunny Pradhan <sun.pradh@gmail.com>
2022-10-28 02:33:21 +0200

    refactoring for the magnetic part

--- hamiltonian.py
+++ hamiltonian.py
@@ -3,10 +3,13 @@ Compute the Hamiltonian
 """
 import numpy as np
 from itertools import product
+from collections.abc import Callable, Sequence
+
 from basis.basis import Basis, StateLabel
 from group import Group_elem, Irreps
-from typing import Callable
 from plaquette import PlaquetteMel, plaquette_links
+from mytyping import MelIndex, VertexLinks, PlaqVertices
+
 
 def electric_single_link_fn(
         generating_set: list[Group_elem],
@@ -31,81 +34,102 @@ def electric_hamiltonian(
     return electric_values
 
 
-def subindices_list(basis: Basis, state: StateLabel):
+def indices_list(
+        basis: Basis,
+        state: StateLabel
+    ) -> list[Sequence[MelIndex]]:
     """
-    Return a list of all the possible subindices (m, n) for each link
+    Return a list of all the possible subindices (j_i, m_i, n_i) for each link
     given a state with a irrep conf (j_1, ..., j_nlinks)
     """
     # possible subindices for each link
-    subindices = [
-        list(product(range(basis.irreps.dim(j)), repeat=2))
+    return [
+        list(
+            product([j], range(basis.irreps.dim(j)), range(basis.irreps.dim(j)))
+        )
         for j in state.irreps
     ]
-    # list of all the possible combination of subindices
-    return list(product(*subindices))
+
+
+def subindices_vertex(
+        vertex: VertexLinks,
+        jmns: Sequence[MelIndex]
+    ) -> tuple[int, int, int, int]:
+    """Return the indices (m1, m2, n3, n4) for a vertex v"""
+    return (
+        jmns[vertex[0]][1],
+        jmns[vertex[1]][1],
+        jmns[vertex[2]][2],
+        jmns[vertex[3]][2]
+    )
+
+
+def compute_psi(
+        psi: np.ndarray,
+        vertex: VertexLinks,
+        jmns: Sequence[MelIndex]
+    ) -> float:
+    m1, m2, n3, n4 = subindices_vertex(vertex, jmns)
+    return psi[m1][m2][n3][n4]
+
+
+def compute_psi_plq(
+        basis: Basis,
+        state: StateLabel,
+        plaquette: PlaqVertices,
+        jmns: Sequence[MelIndex]
+    ) -> float:
+    result = 1
+    for v_i in plaquette:
+        vertex = basis.vertices[v_i]
+        psi = basis(state)[v_i]
+        result = result * compute_psi(psi, vertex, jmns)
+    return result
 
 
 def magnetic_hamiltonian_mel(
         basis: Basis,
         bra: StateLabel,
         ket: StateLabel,
-        plaquettes: list[tuple[int]],
-        wilsonloop_data: PlaquetteMel
+        plaquettes: list[PlaqVertices],
+        plaquette_mels: PlaquetteMel
     ):
-    # We are gonna use a brute force method
-
     # plaquette links
-    plq_links = plaquette_links(basis.vertices, plaquettes)
+    plaqs_links = plaquette_links(basis.vertices, plaquettes)
 
     # subindices (m_i, n_i) for the bra and ket states
-    bra_iter = subindices_list(basis, bra)
-    ket_iter = subindices_list(basis, ket)
+    bra_indices_list = indices_list(basis, bra)
+    ket_indices_list = indices_list(basis, ket)
 
-    def jmn_vertex(vertex: list[int], jmns: tuple[tuple[int, int, int], ...]):
-        return (
-            jmns[vertex[0]][1],
-            jmns[vertex[1]][1],
-            jmns[vertex[2]][2],
-            jmns[vertex[3]][2]
-        )
+    # We are gonna use a brute force method:
+    # Cycle over all the possible subindices (m_i, n_i) given (j_1, ..., j_nlinks)
+    # both for the bra and ket states
 
-    def compute_psi(psi: np.ndarray, vertex: list[int], jmns: tuple[tuple[int, int, int], ...]):
-        m1, m2, n3, n4 = jmn_vertex(vertex, jmns)
-        return psi[m1][m2][n3][n4]
-
-    def compute_psi_plq(basis: Basis, state: StateLabel, plaquette: list[tuple[int, ...]], jmns):
-        result = 1
-        for v_i in plaquette:
-            vertex = basis.vertices[v_i]
-            psi = basis(state)[v_i]
-            result = result * compute_psi(psi, vertex, jmns)
-        return result
-
-
-    # Cycle over all the bra subindices given (j_1, ..., j_nlinks)
+    # Cycle over all the possible plaquettes
     result = 0
-    for vertices, links in zip(plaquettes, plq_links):
-        # Cycle over all the possible plaquettes
-        bjs = tuple(bra.irreps[link] for link in links)
-        kjs = tuple(ket.irreps[link] for link in links)
-        WL = wilsonloop_data.select(bjs, kjs, as_list=False)
+    for p_vertices, p_links in zip(plaquettes, plaqs_links):
+        bra_plaq_js = tuple(bra.irreps[link] for link in p_links)
+        ket_plaq_js = tuple(ket.irreps[link] for link in p_links)
+        # one-plaquette Wilson loop
+        WL = plaquette_mels.select(bra_plaq_js, ket_plaq_js, as_list=False)
+
+        # skip this plaquette if it has no nonzero matrix elements
         if not WL:
             continue
-        for bra_mns in bra_iter:
-            # jmn indices for the bra state
-            bra_jmn = tuple((j, *mn) for j, mn in zip(bra.irreps, bra_mns) )
-            bra_jmn_plq = tuple(jmn for link, jmn in enumerate(bra_jmn) if link in links)
-            bra_jmn_out = tuple(jmn for link, jmn in enumerate(bra_jmn) if link not in links)
 
-            # check if there are matrix elements of C with the given bra state
+        non_p_links = [link for link in range(basis.nlinks) if link not in p_links]
+
+        for bra_jmn in product(*bra_indices_list):
+            bra_jmn_plq = tuple(bra_jmn[link] for link in p_links)
+            bra_jmn_out = tuple(bra_jmn[link] for link in non_p_links)
+
+            # check if there are non-zero matrix elements of WL to sum over
             if bra_jmn_plq not in WL:
                 continue
 
-            for ket_mns in ket_iter:
-                # jmn indicies for the ket state
-                ket_jmn = tuple( (j, *mn) for j, mn in zip(ket.irreps, ket_mns) )
-                ket_jmn_plq = tuple(jmn for link, jmn in enumerate(ket_jmn) if link in links)
-                ket_jmn_out = tuple(jmn for link, jmn in enumerate(ket_jmn) if link not in links)
+            for ket_jmn in product(*ket_indices_list):
+                ket_jmn_plq = tuple(ket_jmn[link] for link in p_links)
+                ket_jmn_out = tuple(ket_jmn[link] for link in non_p_links)
 
                 # check if the indices outside the plaquette are the same
                 if bra_jmn_out != ket_jmn_out:
@@ -117,8 +141,8 @@ def magnetic_hamiltonian_mel(
                 C = WL[bra_jmn_plq][ket_jmn_plq]
 
                 # product of the gauge inv. coeff. for the bra and ket state
-                psi_bra = np.conj(compute_psi_plq(basis, bra, vertices, bra_jmn))
-                psi_ket = compute_psi_plq(basis, ket, vertices, ket_jmn)
+                psi_bra = np.conj(compute_psi_plq(basis, bra, p_vertices, bra_jmn))
+                psi_ket = compute_psi_plq(basis, ket, p_vertices, ket_jmn)
 
                 # add to the sum
                 result = result + (psi_bra * C * psi_ket)
--- mytyping.py
+++ mytyping.py
@@ -1,17 +1,26 @@
 from group import Group_elem
-from typing import Iterable, Callable
+from collections.abc import Sequence, Callable
 
-IrrepFn = Callable[[Group_elem], float | Iterable[float]]
+IrrepFn = Callable[[Group_elem], float | Sequence[float]]
 IrrepIndex = int
-Link = int
 IrrepConf = tuple[IrrepIndex, ...]
-VertexLinks = tuple[Link, Link, Link, Link]
-Vector = Iterable[float]
+Vector = Sequence[float]
 InvariantSpace = list[Vector]
-
-# - Index format in the irrep basis (j, m, n)
-MelIndex = tuple[int, int, int]
-# - Index format for a plaquette state (j_1, m_1, n_1; ...; j_4, m_4, n_4)
-PlaqIndex = tuple[MelIndex, MelIndex, MelIndex, MelIndex]
-# - Group tuple (g_1, g_2, g_3, g_4)
+# Group tuple (g_1, g_2, g_3, g_4)
 GroupTuple = tuple[Group_elem, Group_elem, Group_elem, Group_elem]
+
+# Each link is labeled by an int
+Link = int
+# Each vertex is specified a 4-tuple of links,
+# from the right in the counterclockwise direction
+VertexLinks = tuple[Link, Link, Link, Link]
+# Each vertex of a lattice is labeled by an int
+Vertex = int
+# Each plaquette is specified by a list of 4 vertices,
+# from the bottom left in the counterclockwise direction
+PlaqVertices = tuple[Vertex, Vertex, Vertex, Vertex]
+
+# Index format in the irrep basis (j, m, n)
+MelIndex = tuple[int, int, int]
+# Index format for a plaquette state (j_1, m_1, n_1; ...; j_4, m_4, n_4)
+PlaqIndex = tuple[MelIndex, MelIndex, MelIndex, MelIndex]
